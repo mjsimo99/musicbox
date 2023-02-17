b7feb725536531090207314f1bc69d8eb86319d3
mjsimo99 <mohashark21@gmail.com>
2023-02-17 17:30:55 +0100

    update added

--- resources/views/bands/edit.blade.php
+++ resources/views/bands/edit.blade.php
@@ -4,3 +4,22 @@
 
 <div class="card" style="margin:20px;">
     <div class="card-header">Edit band</div>
+    <div class="card-body">
+
+        <form action="" method="post">
+            {{!! csrf_field() !!}}
+            @method('PUT')
+            {{-- @method('PATCH') --}}
+            <input type="hidden" name="id" id="id" value="{{ $bands->id }}" id="id" /> 
+            <label>Name</label>
+            <input type="text" name="name" id="name" value="{{ $bands->name }}" id="name" />
+            <label>Genre</label>
+            <input type="text" name="genre" id="genre" value="{{ $bands->genre }}" id="genre" />
+            <input type="submit" value="Update" class="btn btn-primary" />
+                  
+        </form>
+
+
+
+    </div>
+</div>
